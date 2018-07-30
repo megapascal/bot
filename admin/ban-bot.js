8433e45051e1a8f1cee1e46ad9426d29f734dfd5
megapascal <39701838+megapascal@users.noreply.github.com>
2018-07-30 20:59:52 +0500

    chinese assholes will be banned
    please check if it works at first!

--- admin/ban-bot.js
+++ admin/ban-bot.js
@@ -1,10 +1,20 @@
 module.exports = async ({ message, kickChatMember, replyWithSticker }) => {
+  let fullName = member.first_name;
+     if (member.last_name) {
+        fullName += ` ${member.last_name}`;
+      }
+  fullName = fullName.toLowerCase();
   const blacklistes = message.new_chat_members.filter((member) => (
-    member.is_bot ||
-    member.first_name.startsWith(`╋VX,QQ（同号）`) ||
-    member.first_name.startsWith('╋VX(QQ)')
-  ))
-
+    member.is_bot || fullName.includes(`╋vx`)
+        || fullName.includes(`(stockcraft at hotmail.com)`)
+        || fullName.includes(`we can add 1000+ 10000+`)
+        || fullName.includes(`any number real and active members`)
+        || fullName.includes(`leave no join alerts,quality and quantity guaranteed`)
+  ){
+      await kickChatMember(member.id)
+    }
+    await replyWithSticker('CAADAgADrQADVsXcB3r5IHrTHq6QAg')
+                                                      }
   if (blacklistes.length > 0) {
     for (const member of blacklistes) {
       await kickChatMember(member.id)
